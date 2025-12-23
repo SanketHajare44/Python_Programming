f04739b6c2a19ea28f8003da0938d1860029d6f8
SanketHajare44 <sankethajare44@gmail.com>
2025-12-23 07:44:37 +0530

    Check size of datatype

---
+++ Conceptual/Size.py
@@ -0,0 +1,11 @@
+import sys
+
+No = 1112333333333333333333333333333333333333333333333333333333333       # Int
+
+Marks = 90.00   # float
+
+Name = "Rahul"  # str
+
+print(sys.getsizeof(No))
+print(sys.getsizeof(Marks))
+print(sys.getsizeof(Name))
