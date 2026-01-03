2382bf6ce8bac705e94faaf3a35f916dbddc1d94
SanketHajare44 <sankethajare44@gmail.com>
2026-01-03 23:58:01 +0530

    function implementation

---
+++ Conceptual/Function_4.py
@@ -0,0 +1,21 @@
+print("Enter first number")
+no1 = int(input())
+
+print("Enter Second number")
+no2 = int(input())
+
+Ans = no1 * no2
+
+print("Multipication is : ",Ans)
+
+#################################################
+
+print("Enter first number")
+no1 = int(input())
+
+print("Enter Second number")
+no2 = int(input())
+
+Ans = no1 * no2
+
+print("Multipication is : ",Ans)
