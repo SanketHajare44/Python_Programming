9e64c4b415e9b3004699ad77912ecc717be12224
SanketHajare44 <sankethajare44@gmail.com>
2026-01-04 23:55:02 +0530

    Demonstrate Python function type

---
+++ Conceptual/FunctionDemo13.py
@@ -0,0 +1,16 @@
+
+def Phoenix():
+    print("Inside Phoenix")
+    
+    
+    def Zara():
+        print("Inside Zara")
+    
+    Zara()
+    
+
+def main():
+    Phoenix()             
+
+if __name__ == "__main__":
+    main()
