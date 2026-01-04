9068047df936ba9a315534640f3c5ec6269650fb
SanketHajare44 <sankethajare44@gmail.com>
2026-01-04 23:47:31 +0530

    DataTypes

---
+++ Conceptual/RevisionAll.py
@@ -0,0 +1,14 @@
+print("-"*20)
+
+arr = {10,20,30,40,10}
+print(arr)
+print(type(arr))
+
+print("-"*20)
+
+book = {"name":"let us c", "price":350, "Author":"Y Kanetkar"}
+print(book)
+print(type(book))
+print(book["price"])
+
+print("-"*20)
