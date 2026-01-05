f6c6c4305d2ecc2684e96d0979542300402a9d99
SanketHajare44 <sankethajare44@gmail.com>
2026-01-05 23:41:56 +0530

    Marvellous module is imported in this

---
+++ Conceptual/ModuleClient.py
@@ -0,0 +1,15 @@
+import Marvellous
+
+print("Inside client : ",__name__)
+
+print("value of PI is :",Marvellous.PI)
+
+Result = 0
+
+Result = Marvellous.Add(10,20)
+
+print("Addition is : ",Result)
+
+Result = Marvellous.Sub(20,10)
+
+print("Substraction  is : ",Result)
