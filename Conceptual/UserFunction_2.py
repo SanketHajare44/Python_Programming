a61d5625e65bb2162a910aedeb1a6bd44eafab05
SanketHajare44 <sankethajare44@gmail.com>
2026-01-05 23:46:11 +0530

    userdefined function demonstrate

---
+++ Conceptual/UserFunction_2.py
@@ -0,0 +1,13 @@
+def Multiplication(Value1, value2):
+    Ans = 0  # local variable
+    Ans = Value1 * value2
+    return Ans
+
+print("Demo application")
+
+No1 = 10
+No2 = 11
+Result = 0
+
+Result = Multiplication(No1,No2)
+print("Multiplication is : ",Result)
