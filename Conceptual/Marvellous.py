fa7c5de0c4a92659b69ef0ce48e56c80c44b6f33
SanketHajare44 <sankethajare44@gmail.com>
2026-01-05 23:42:34 +0530

    module is created

---
+++ Conceptual/Marvellous.py
@@ -0,0 +1,14 @@
+PI = 3.14
+
+print("Inside Module",__name__)
+
+def Add(no1, no2):
+    Ans = 0
+    Ans = no1 + no2
+    return Ans
+
+def Sub(no1, no2):
+    Ans = 0
+    Ans = no1 - no2
+    return Ans
+
