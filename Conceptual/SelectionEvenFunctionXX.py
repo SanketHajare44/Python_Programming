0008f7cd1f422e185f7394d2e839311006b01beb
SanketHajare44 <sankethajare44@gmail.com>
2026-01-10 19:27:02 +0530

    check even odd using function witout starter function

---
+++ Conceptual/SelectionEvenFunctionXX.py
@@ -0,0 +1,15 @@
+# procedural
+
+def CheckEven(no):
+    if(no % 2 == 0):
+        print("It is even.")
+    else:
+        print("It is odd.")
+
+
+Value = 0
+
+print("enter number : ")
+Value = int(input())
+
+CheckEven(Value)
