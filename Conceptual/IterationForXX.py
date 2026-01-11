3a4acbc976d9c731d43c441d0e4087a46a9a2e86
SanketHajare44 <sankethajare44@gmail.com>
2026-01-11 22:04:27 +0530

    Iteration using for loop

---
+++ Conceptual/IterationForXX.py
@@ -0,0 +1,13 @@
+# sequence
+
+print("Jay Ganesh")
+print("Jay Ganesh")
+print("Jay Ganesh")
+print("Jay Ganesh")
+
+print("_____________________________________")
+
+# Iteration
+
+for i in range(4):
+    print("Jay Ganesh")
