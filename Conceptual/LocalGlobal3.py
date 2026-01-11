874055e9e00b4ec8b83d4c65e75a42d0f0e2e63a
SanketHajare44 <sankethajare44@gmail.com>
2026-01-11 21:59:56 +0530

    concept of local and global variable

---
+++ Conceptual/LocalGlobal3.py
@@ -0,0 +1,13 @@
+no = 11          # global
+
+def fun():
+    no = 21
+    print("Value of no from fun is : ", no)
+    no =  no + 1
+    print("Value of no from fun is : ", no)
+
+print("Value of no is :",no)
+
+fun()
+
+print("Valuue of no is :",no)
