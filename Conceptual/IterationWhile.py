aa0228f05505570e0ebd1f24f6dbb64ec1b7105e
SanketHajare44 <sankethajare44@gmail.com>
2026-01-11 22:05:33 +0530

    Iteration using while loop

---
+++ Conceptual/IterationWhile.py
@@ -0,0 +1,16 @@
+# sequence
+
+print("Jay Ganesh")
+print("Jay Ganesh")
+print("Jay Ganesh")
+print("Jay Ganesh")
+
+print("_____________________________________")
+
+# Iteration
+
+i = 1
+
+while(i < 5):
+    print("Jay Ganesh")
+    i = i + 1
