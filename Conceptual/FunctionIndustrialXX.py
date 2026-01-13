1ef34b8873b4b9cdd8febfde26e2c12297f82bf2
SanketHajare44 <sankethajare44@gmail.com>
2026-01-13 10:58:03 +0530

    function is defined as industrial way

---
+++ Conceptual/FunctionIndustrialXX.py
@@ -0,0 +1,23 @@
+# procedural
+
+def CheckEven(no):
+    return (no % 2 == 0)
+
+
+def main():
+    Value = 0
+    Ret = False
+
+    print("Enter number : ")
+    Value = int(input())
+
+    Ret = CheckEven(Value)
+    
+    if(Ret == True):
+        print("It is Even")
+    else:
+        print("It is odd")
+
+
+if __name__ == "__main__":
+    main()
