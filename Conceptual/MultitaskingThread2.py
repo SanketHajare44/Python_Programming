3076f2adcd87dfd1dd35af528db0ee5a7d68da5e
SanketHajare44 <sankethajare44@gmail.com>
2026-01-17 23:08:51 +0530

    single thread is created

---
+++ Conceptual/MultitaskingThread2.py
@@ -0,0 +1,18 @@
+# Single thread is created
+
+import threading
+
+def Display():
+    print("Inside Display function : ",threading.get_ident())
+
+def main():
+    print("Inside main : ",threading.get_ident())
+    
+    t = threading.Thread(target=Display)
+    t.start()
+
+    print("End of main")
+
+
+if __name__ == "__main__":
+    main()
