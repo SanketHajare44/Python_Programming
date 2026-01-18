8bbad696a776fb224b4962b133fe00c0bb6f740c
SanketHajare44 <sankethajare44@gmail.com>
2026-01-18 23:11:53 +0530

    normal function is writed before doing exception

---
+++ Conceptual/ExceptionDemo1.py
@@ -0,0 +1,14 @@
+
+def main():
+    print("Enter first number : ")
+    no1 = int(input())
+
+    print("Enter Second number : ")
+    no2 = int(input())
+
+    Ans = no1 / no2
+
+    print("Division is : ",Ans)
+
+if __name__ == "__main__":
+    main()
