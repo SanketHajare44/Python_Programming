89891d1d0847548924b80bba38ae3877706a68df
SanketHajare44 <sankethajare44@gmail.com>
2026-01-20 16:38:22 +0530

    Add solution for Assignment 9 Q1

---
+++ Assignments/Assignment_9/program9_1.py
@@ -0,0 +1,10 @@
+# Wrte a program which contains one function named as Display() that prints "Jay Ganesh" on Console
+
+def Display():
+    print("Jay Ganesh...")
+
+def main():
+    Display()
+
+if __name__ == "__main__":
+    main()
