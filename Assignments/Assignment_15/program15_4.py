9729247fcf09a73923bd1507b3b3134b84fbb40e
SanketHajare44 <sankethajare44@gmail.com>
2026-01-23 16:49:57 +0530

    Add solution for Assignment 15 Q4

---
+++ Assignments/Assignment_15/program15_4.py
@@ -0,0 +1,28 @@
+# # Write a lambda function using reduce() which accepts list of numbers and returns addition of numbers
+
+addition = lambda No1, No2 : No1 + No2 
+
+def reduceX(Task,Elements):
+    Result = 0
+    for i in Elements:
+        Result = Task(Result,i)
+    return Result
+
+def main():
+    Arr = list()
+    Ret = 0
+    print("Enter the number of elements")
+    value = int(input())
+
+    for i in range(value):
+        Data = int(input("Enter the element : "))
+        Arr.append(Data)
+
+    print("Original list : ",Arr)
+
+    Ret = reduceX(addition,Arr)
+    print("Addition is :", Ret)
+
+
+if __name__ == "__main__":
+    main()
