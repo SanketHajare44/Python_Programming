2a09f35d3da571cac26849a5ff993474d583b57f
SanketHajare44 <sankethajare44@gmail.com>
2026-01-23 16:49:51 +0530

    Add solution for Assignment 15 Q5

---
+++ Assignments/Assignment_15/program15_5.py
@@ -0,0 +1,29 @@
+'''Write a lambda function using reduce() which accepts list of 
+   numbers and returns maximum of number of elements'''
+
+addition = lambda No1, No2 : No1 if No1 > No2 else No2
+
+def reduceX(Task,Elements):
+    Result = Elements[0]
+    for i in Elements:
+        Result = Task(Result,i)
+    return Result
+
+def main():
+    Arr = list()
+    Ret = 0
+    print("Enter the number of elements")
+    value = int(input())
+
+    for i in range(value):
+        Data = int(input("Enter the element : "))
+        Arr.append(Data)
+
+    print("Original list : ",Arr)
+
+    Ret = reduceX(addition,Arr)
+    print("Maximum number is  :", Ret)
+
+
+if __name__ == "__main__":
+    main()
