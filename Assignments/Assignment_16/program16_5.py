96a32be25a6d91579f402c262ddee18a639ed049
SanketHajare44 <sankethajare44@gmail.com>
2026-01-24 15:12:28 +0530

    Add solution for Assignment 16 Q5

---
+++ Assignments/Assignment_16/program16_5.py
@@ -0,0 +1,11 @@
+''' Write a program which display 10 t0 1 on screen. '''
+
+def Display():
+    for i in range(10,0,-1):
+        print(i)
+
+def main():
+    Display()
+
+if __name__ == "__main__":
+    main()
