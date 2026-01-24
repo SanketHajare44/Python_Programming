e1f80954973c0cb0070666c5b3a0b79d0ab86c5d
SanketHajare44 <sankethajare44@gmail.com>
2026-01-24 15:11:58 +0530

    Add solution for Assignment 16 Q1

---
+++ Assignments/Assignment_16/program16_1.py
@@ -0,0 +1,11 @@
+''' Write a program which contains one function named as fun(). That function
+    should display "Hello from fun" on console.'''
+
+def fun():
+    print("Hello from fun")
+
+def main():
+    fun()
+
+if __name__ == "__main__":
+    main()
