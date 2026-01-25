ba7831d0c9849b22c15157204071f184347cbc2b
SanketHajare44 <sankethajare44@gmail.com>
2026-01-25 15:56:53 +0530

    UserDefined Module

---
+++ Assignments/Assignment_17/Arithmetic.py
@@ -0,0 +1,20 @@
+
+def Add(Value1, Value2):
+    Ans = 0
+    Ans = Value1 + Value2
+    return Ans
+
+def Sub(Value1, Value2):
+    Ans = 0
+    Ans = Value1 - Value2
+    return Ans
+
+def Multi(Value1, Value2):
+    Ans = 0
+    Ans = Value1 * Value2
+    return Ans
+
+def Div(Value1, Value2):
+    Ans = 0
+    Ans = Value1 / Value2
+    return Ans
