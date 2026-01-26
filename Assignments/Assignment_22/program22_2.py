f34a1c0b4d9e60840e468e74d5e260e137d3310d
SanketHajare44 <sankethajare44@gmail.com>
2026-01-26 19:44:58 +0530

    Add solution for Assignment 22 Q2

---
+++ Assignments/Assignment_22/program22_2.py
@@ -0,0 +1,55 @@
+# ''' Write a Python program to implement a class named Circle with the following requirements:
+
+#     * The class should contain three instance variables: Radius, Area, and Circumference.
+#     * The class should contain one class variable named PI. initialized to 3. 14.
+#     * Define a constructor ( init) that initializes all instance variables to 0. 0.
+
+#     * Implement the following instance methods:
+#         Accept () - accepts the radius of the circle from the user.
+#         CalculateArea () - calculates the area of the circle and stores it in the Area variable.
+#         CalculateCircumference() - calculates the circumference of the circle and stores it in the Circumference variable.
+#         Display () - displays the values of Radius, Area, and Circumference.
+
+#     * Create multiple objects of the Circ le class and invoke all the instance methods for each object.
+# '''
+
+class Circle:
+    PI = 3.14
+    
+
+    def __init__(self):
+        self.Radius = 0.0
+        self.Area = 0.0
+        self.Circumference = 0.0
+    
+    def Accept(self):
+        print("Enter the Radius")
+        self.Radius = float(input())
+
+    def CalculateArea(self):
+        self.Area = Circle.PI * self.Radius * self.Radius
+
+    def CalculateCircumference(self):
+        self.Circumference = 2 * Circle.PI * self.Radius
+
+    def Display(self):
+        print(self.Radius) 
+        print(self.Area)
+        print(self.Circumference)
+
+def main():    
+    obj1 = Circle()
+    obj2 = Circle()
+
+    obj1.Accept()
+    obj1.CalculateArea()
+    obj1.CalculateCircumference()
+    obj1.Display()
+
+    obj2.Accept()
+    obj2.CalculateArea()
+    obj2.CalculateCircumference()
+    obj2.Display()
+
+if __name__ == "__main__":
+    main()
