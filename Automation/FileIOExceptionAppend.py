61a03b3ead70daa9ce5de3b7e5a35752c62e9cd6
SanketHajare44 <sankethajare44@gmail.com>
2026-01-31 23:48:01 +0530

    File is opened i append mode 'a'

---
+++ Automation/FileIOExceptionAppend.py
@@ -0,0 +1,19 @@
+
+def main():
+    fobj = None
+    try:
+        fobj = open("Hello.txt","a")     # opened for write with append mode (fobj is handle) 
+        print("File gets succesfully opened")
+
+        fobj.write("Python Automation")     # write method is used
+
+        fobj.close()                # file is closed using close() method
+
+    except FileNotFoundError:           # specific  exception handled
+        print("Unable to open file as there is no such file")
+    finally:       
+        print("End of application")     # always execute
+
+
+if __name__ == "__main__":        # Also called as dunder key word
+    main()
