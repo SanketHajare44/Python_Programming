2e396388c039ebefcdcb091fff2d4d95957ddf93
SanketHajare44 <sankethajare44@gmail.com>
2026-01-31 23:49:30 +0530

    check the give input is relative path or absolute path

---
+++ Automation/FileIOPath.py
@@ -0,0 +1,15 @@
+import os
+
+def main():
+    FileName = input("Enter the name of file : ")
+    
+    Ret = os.path.isabs(FileName)       # check path is Absoulute or not
+    
+    if(Ret == True):
+        print("It is absolute path")
+    else:
+        print("It is relative path")
+
+
+if __name__ == "__main__":
+    main()
