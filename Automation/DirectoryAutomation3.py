ff5c5cd5147b5a50f8a037b8a7d12afd926162ab
SanketHajare44 <sankethajare44@gmail.com>
2026-02-01 23:25:59 +0530

    DirectoryAutomation2.py error handled here

---
+++ Automation/DirectoryAutomation3.py
@@ -0,0 +1,38 @@
+import os
+import sys
+
+def DirectoryScanner(DirName = "Marvellous"):
+    Ret = False
+    Ret = os.path.exists(DirName)
+    if(Ret == False):
+        print("There is no such directory")
+        return
+    
+    Ret = os.path.isdir(DirName)
+    if(Ret == False):
+        print("It is not a directory")
+        return
+    
+    for FolderName, SubFolder, FileName in os.walk(DirName):
+        for fname in FileName:
+            fname = os.path.join(FolderName,fname) # previous error here handled
+
+            print("File name : ",fname)
+            print("File Size : ",os.path.getsize(fname))
+            
+
+def main():
+    border = "-"*50
+    print(border)
+    print("----------Marvellous Directory Automation---------")
+    print(border)
+    
+    if(len(sys.argv) != 2):
+        print("Invalid number of arguments")
+        print("Please specify the name of directory")
+        return
+    
+    DirectoryScanner(sys.argv[1])
+
+if __name__ == "__main__":
+    main()
