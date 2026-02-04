43a305dfda2865ef26be752fa97a2bd378fb4da7
SanketHajare44 <sankethajare44@gmail.com>
2026-02-04 22:55:10 +0530

    Exception hanled when file is open for read but file is not exists

---
+++ Automation/FileIOExceptionReadX.py
@@ -0,0 +1,20 @@
+
+def main():
+    fobj = None
+    try:
+        fobj = open("Hello.txt","r")     # opened for 'Read' (fobj is handle)
+        print("File gets succesfully opened")
+
+        Data = fobj.read(6)          # read the first '6 byte' from Hello.txt
+        print("Data from file is : ",Data)
+
+        fobj.close()                # file is closed using 'close()' method
+
+    except FileNotFoundError:           # 'specific'  exception handled
+        print("Unable to open file as there is no such file")
+    finally:       
+        print("End of application")     # always execute
+
+
+if __name__ == "__main__":        # Also called as dunder key word
+    main()
