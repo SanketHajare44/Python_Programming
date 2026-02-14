97c00795734c67656fe6f34caa784ac23825622f
SanketHajare44 <sankethajare44@gmail.com>
2026-02-14 23:48:08 +0530

    condition aplied in the BackupFiles() method if same file name is not exist

---
+++ Automation/DataShield/5DataShieldCheckSumUpdated.py
@@ -0,0 +1,117 @@
+
+import psutil
+import sys
+import os
+import time
+import schedule
+import shutil
+import hashlib
+
+def calculate_hash(path):
+    hobj = hashlib.md5()
+
+    fobj = open(path, "rb")
+
+    while True:
+        data = fobj.read(1024)
+        if not data:
+            break
+        else:
+            hobj.update(data)
+    
+    fobj.close()
+
+    return hobj.hexdigest
+
+def BackupFiles(Source, Destination):
+    copied_files = []
+
+    print("Creating the Backup folder for backup process")
+
+    os.makedirs(Destination, exist_ok = True)
+    
+    for root, dirs, files in os.walk(Source):
+        for file in files:
+            src_path = os.path.join(root,file)
+
+            relative = os.path.relpath(src_path, Source)
+            dest_path = os.path.join(Destination, relative)
+
+            os.makedirs(os.path.dirname(dest_path), exist_ok = True)
+
+
+            # Copy the files if its new 
+            if(not os.path.exists(dest_path)):
+                shutil.copy2(src_path,dest_path)
+                copied_files.append(relative)
+    
+    return copied_files
+
+def MarvellousDataShieldStart(Source = "Data"):
+    BackupName = "MarvellousBackup"
+
+    print("Backup process started succesfully at ",time.ctime())
+
+    files = BackupFiles(Source, BackupName)
+
+    print("Report about the backup")
+
+    for name in files:
+        print(name)
+
+
+
+def main():
+
+    Border = "-"*50
+    print(Border)
+    print("-----------Marvellous Data Shield System-----------")
+    print(Border)
+
+    if(len(sys.argv) == 2):
+        if(sys.argv[1] == "--h" or sys.argv[1] == "--H"):
+            print("This script is used to to : ")
+            print("1 : Takes auto backup at given time")
+            print("2 : Backup only new and updated files")
+            print("3 : Create an archive ofthe backup perodically")
+
+        elif(sys.argv[1] == "--u" or sys.argv[1] == "--U"):
+            print("Use the automationc script as")
+            print("ScriptName.py TimeInterval SourceDirectory")
+            print("TimeInterval : The time in minutes for perodic scheduling")
+            print("SourceDirectory : Name of directory to backed up")
+
+        else:
+            print("Unable to proceed as there is no such option")
+            print("Please use --u or --h to get more details")
+    
+    # python Demo.py 5 Data
+    elif(len(sys.argv) == 3):
+        print("Inside project logic")
+        print("Time Interval : ",sys.argv[1])
+        print("Directory name : ",sys.argv[2])
+
+        print("Data Shield system started succesfully")
+        print("Time interval in minutes : ",sys.argv[1])
+        print("Press Ctrl + C to stop the execution")
+
+        # Apply the schedular
+        schedule.every(int(sys.argv[1])).minutes.do(MarvellousDataShieldStart, sys.argv[2])
+
+        # Wait till abort
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+    
+    else:
+        print("Invalid number of command line argument")
+        print("Unable to proceed as there is no such option")
+        print("Please use --u or --h to get more details")
+
+
+    print(Border)
+    print("------------Thank you for using script------------")
+    print(Border)
+
+if __name__ == "__main__":
+    main()
