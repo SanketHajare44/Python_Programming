8360389df4c0f5138efb2501b0e0ab4f6b85ff2f
SanketHajare44 <sankethajare44@gmail.com>
2026-02-15 23:53:58 +0530

    Confusion matrix used

---
+++ Machine_Learning/Confusion_Matrix.py
@@ -0,0 +1,21 @@
+from sklearn.metrics import confusion_matrix
+
+
+def main():
+
+    # 1 : Positive
+    # 0 : Negative
+
+    Actual =    [1,0,1,1,1,0,1,0,0,1]
+
+    Predicted = [1,0,0,1,1,1,1,1,0,1]
+
+    print("Actual data : ",Actual)
+    print("Predected data : ",Predicted)
+
+    con_mat = confusion_matrix(Actual,Predicted)
+
+    print(con_mat)
+
+if __name__ == "__main__":
+    main()
