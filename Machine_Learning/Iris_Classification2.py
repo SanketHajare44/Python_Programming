a24022020f79254d51f92bb9866d0e3b736dff10
SanketHajare44 <sankethajare44@gmail.com>
2026-02-15 23:55:20 +0530

    Iris data case study

---
+++ Machine_Learning/Iris_Classification2.py
@@ -0,0 +1,19 @@
+# Best way to display Iris dataset on console
+
+from sklearn.datasets import load_iris
+
+
+def main():
+    print("Iris classification case study")
+
+    Dataset = load_iris()
+
+    # Meta Data of Dataset
+    print("Independent variables are : ")
+    print(Dataset.feature_names)
+
+    print("Dependent Variables are : ")
+    print(Dataset.target_names)
+
+if __name__ == "__main__":
+    main()
