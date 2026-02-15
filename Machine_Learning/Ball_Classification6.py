33b1e56f61eb39f8bbf78d937ac517481c601dea
SanketHajare44 <sankethajare44@gmail.com>
2026-02-15 23:53:18 +0530

    print on console result in nameing

---
+++ Machine_Learning/Ball_Classification6.py
@@ -0,0 +1,49 @@
+
+from sklearn import tree
+
+
+# Rough = 1
+# Smooth = 0
+
+# Tennis = 1
+# Cricket = 2
+
+
+def main():
+    print("Ball classification case study")
+
+    # Original Encoded Dataset
+    # Independent variables
+    X =[[35,1],[47,1],[90, 0], [48,1], [90, 0],[35,1],[92, 0],[35,1],[35,1],[35,1],[96,0],[43,1],[110, 0],[35,1],[95,0]]
+
+    # Dependent variables
+    Y = [1,1,2,1,2,1,2,1,1,1,2,1,2,1,2]
+
+    #Independent Variables for training
+    Xtrain = [[35,1],[47,1],[90, 0], [48,1], [90, 0],[35,1],[92, 0],[35,1],[35,1],[35,1],[96,0],[43,1],[110, 0]]
+
+    #Independent Variables for testing
+    Xtest = [[35,1],[95,0]]
+
+    # Dependent variables for training
+    Ytrain = [1,1,2,1,2,1,2,1,1,1,2,1,2]
+
+    # Dependent variables for testing
+    Ytest = [1,2]
+
+    modelobj = tree.DecisionTreeClassifier()
+
+    trainedmodel = modelobj.fit(Xtrain,Ytrain)
+
+    Result = trainedmodel.predict([[35,1]])
+
+    print(type(Result))
+    
+    if Result == 1:
+        print("Object looks like Tennis Ball")
+    elif Result == 2:
+        print("Object looks like Cricket Ball")
+
+
+if __name__ == "__main__":
+    main()
