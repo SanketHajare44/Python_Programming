837f8ae6d90a2714de3e5c3561e2f89ecdc170f9
SanketHajare44 <sankethajare44@gmail.com>
2026-02-22 14:56:33 +0530

    DataFrame is like 2D array (collection of series called as DataFrame)

---
+++ Machine_Learning/Pandas_Dataframe2.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+def main():
+    Data = {
+        "Name" : ["Sagar","Amit","Pooja"],
+        "Age" : [23,26,25],
+         "City" : ["Pune","Mumbai","Satara"]
+    }
+
+    dobj = pd.DataFrame(Data)                       # Dataframe is like 2D Array    (Dataframe is collection of series)
+
+    print(dobj)
+
+    print(dobj["City"])
+
+
+if __name__ == "__main__":
+    main()
+
+# as python they only consider only numeric data type in library
