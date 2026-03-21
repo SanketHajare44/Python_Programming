0ddad1e8bd0f0af6e548aba9e75afc2859da6404
SanketHajare44 <sankethajare44@gmail.com>
2026-03-21 15:15:23 +0530

    feature selection is done here

---
+++ Machine_Learning/73.Mall_CustomerKMeanSelectFeatures_2.py
@@ -0,0 +1,41 @@
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+
+def main():
+
+    #===============================================================
+    #   Step 1 : Load the dataset
+    #===============================================================
+
+    print("Step 1 : Load the dataset")
+    df = pd.read_csv("Mall_Customers.csv")
+
+    print("\nFirst few records : ")
+    print(df.head())
+
+    print("\nShape of dataset : ")
+    print(df.shape)
+
+    print("\nMissing values : ")
+    print(df.isnull().sum())
+
+
+    #===============================================================
+    #   Step 2 : Select Features (Independent)
+    #===============================================================
+
+    print("\nStep 2 : Select Features (Independent)")
+
+    X = df[["AnnualIncome", "SpendingScore"]]
+
+    print("\nSelected features : ")
+    print(X.head())
+
+    print("\nShape of selected features : ")
+    print(X.shape)
+
+if __name__=="__main__":
+    main()
