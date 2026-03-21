fb726420b853a46c1a6a9077480558704168da13
SanketHajare44 <sankethajare44@gmail.com>
2026-03-21 15:14:40 +0530

    Mall customer dataset is loaded

---
+++ Machine_Learning/72.Mall_CustomerKMeanLoadData_1.py
@@ -0,0 +1,28 @@
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+
+def main():
+
+    #===============================================================
+    #   Step 1 : Load the dataset
+    #===============================================================
+
+    print("Step 1 : Load the dataset")
+    df = pd.read_csv("Mall_Customers.csv")
+
+    print("\nFirst few records : ")
+    print(df.head())
+
+    print("\nShape of dataset : ")
+    print(df.shape)
+
+    print("\nMissing values : ")
+    print(df.isnull().sum())
+
+
+
+if __name__=="__main__":
+    main()
