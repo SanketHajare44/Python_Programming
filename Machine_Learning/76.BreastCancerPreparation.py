fac6fd8258db0370a397f51792df43c21896502e
SanketHajare44 <sankethajare44@gmail.com>
2026-03-23 23:49:24 +0530

    Breast cancer data prepration

---
+++ Machine_Learning/76.BreastCancerPreparation.py
@@ -0,0 +1,14 @@
+#   CSV file is created programatically
+
+import pandas as pd 
+from sklearn.datasets import load_breast_cancer
+
+data = load_breast_cancer()
+
+df = pd.DataFrame(data.data,columns=data.feature_names)
+
+df["target"] = data.target
+
+df.to_csv("breast_cancer.csv",index=False)
+
+print("breast_cancer.csv file gets created succesfully")
